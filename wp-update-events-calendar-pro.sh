cf672f145a943418f362903625115b48d82a3c40
Jakob Walk <jjwalk@gmx.de>
2023-04-05 12:57:51 +0200

    adapt script to changed api response

--- wp-update-events-calendar-pro.sh
+++ wp-update-events-calendar-pro.sh
@@ -4,13 +4,28 @@ WP_PATH="/PATH/TO/WORDPRESS"
 LICENSE_KEY=XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 # Get latest plugin versions from api
-curl -LJs -o "plugin_latest.json" "https://pue.tri.be/api/plugins/v2/plugins/latest?plugin=events-calendar-pro"
+curl -LJs -o "plugin_latest.json" "https://pue.tri.be/api/plugins/v2/plugins/latest"
 
-# Get the version number of the latest plugin
-new_version=$(python -c "import json; data = json.load(open('plugin_latest.json')); print([elem for elem in data['plugins'] if elem['plugin_name'] == 'Events Calendar PRO'][0]['plugin_version'])")
+# Define the search substring
+substring="events calendar pro"
+
+# Use curl to retrieve the JSON data from the URL and extract the version and download URL
+version_url=$(python -c "import json; data = json.load(open('plugin_latest.json')); matching_plugins = [elem for elem in data['plugins'] if '$substring'.lower() in elem['plugin_name'].lower()]; print('{0}:{1}'.format(matching_plugins[0]['plugin_version'], matching_plugins[0]['plugin_download_url']))")
+
+# Check if a matching plugin was found
+if [[ "$version_url" == "" ]]; then
+  echo "Plugin not found."
+  exit 1
+fi
+
+# Extract the version and download URL from the version_url string
+IFS=':' read -r new_version download_url <<< "$version_url"
 
 # Get the version number of the currently installed plugin
 installed_version=$(wp plugin get events-calendar-pro --field=version --path="$WP_PATH")
+
+echo "Installed Version: $installed_version"
+echo "New Version: $new_version"
 
 # Compare the version numbers
 version_gt() { test "$(printf '%s\n' "$@" | sort -V | head -n 1)" != "$1"; }
@@ -23,7 +38,6 @@ if version_gt "$new_version" "$installed_version"; then
   echo "Checking for updates for the-events-calendar ..." # new version is often needed for the pro plugin
   wp plugin update "the-events-calendar" --path="$WP_PATH"
   
-  download_url=$(python -c "import json; data = json.load(open('plugin_latest.json')); print([elem for elem in data['plugins'] if elem['plugin_name'] == 'Events Calendar PRO'][0]['plugin_download_url'])")
   filename="events-calendar-pro.${new_version}.zip"
 
   curl -LJs -o "$filename" "${download_url}&key=${LICENSE_KEY}"
